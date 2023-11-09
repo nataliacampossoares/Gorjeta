c8ad994b6da72b6e80ace79a4a05fb5e8e8c99bf
natinha <130059334+nataliacampossoares@users.noreply.github.com>
2023-11-09 08:55:57 -0300

    .

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-function calcular(){
-    let saida = document.getElementById("valorTotal")
-    let valor_conta = document.getElementById("valorConta").value 
-    let qualidade = document.getElementById("qualidadeServico").value 
-    let gorjeta = document.getElementById("tipoConta")
+function calculadora(){
+    let saida_valor_total = document.getElementById("valorTotal")
+    let saida_valor_gorjeta = document.getElementById("tipoConta")
+    let valor_conta = document.getElementById("valorConta").value
+    let gorjeta = document.getElementById("tipoConta").value
 
-    if (qualidade = )
+    if (valortotal === "" || gorjeta === "") {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    var gorjeta_total = valor_conta * gorjeta
+    var valor_final = gorjeta_total(Number) + valor_conta(Number)
+
+    saida_valor_total.innerHTML = valor_final
+    saida_valor_gorjeta.innerHTML = gorjeta_total
+
 }
