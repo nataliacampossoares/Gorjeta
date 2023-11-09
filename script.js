4564db00496cb09153ff5a04e54c25109018e26f
natinha <naticamposs010607@gmail.com>
2023-11-09 06:00:34 -0300

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+function calcular(){
+    let saida = document.getElementById("valorTotal")
+    let valor_conta = document.getElementById("valorConta").value 
+    let qualidade = document.getElementById("qualidadeServico").value 
+    let gorjeta = document.getElementById("tipoConta")
+
+    if (qualidade = )
+}
